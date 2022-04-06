3f584499f7833bcbedee2c083c20e1aa1f840ecd
marwanseerat <marwanseerat06@gmail.com>
2022-04-06 20:38:30 +0300

    my task

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+
+// task1
+console.log("task1:");
 let x=[1,2,3,4,5,6]
 function reverse(x){
     for(let i= x.length-1; i>=0; i--)
         console.log(x[i]);
     }
     reverse(x)
+    
+    console.log(reverse(x));
+  
+    document.getElementById("task1").innerHTML=` the right answer is in the console  ${reverse(x)};`
+
+/////////////////////////////////////////////////////////////////
+// task2
+
 
 
     let y= 'the car drove fast';
@@ -16,6 +27,72 @@ function reverse(x){
          console.log(z[i]);
      }
     }
-    
+    console.log("task2:");
     string(y)
     
+    
+
+    document.getElementById("task2").innerHTML= ` the right answer is in the console  ${string(y)};`
+    /////////////////////////////////////////////////////////////////
+
+
+// task3
+
+function palindrome (str){
+    let d = str.length-1;
+    for(let i =0 ; i<d/2 ;i++){
+        let z=str[i]
+        let a=str[d-i]
+        if(z!=a){
+            return false
+        }
+        return true
+    }
+    
+   
+} 
+console.log("task3:");
+function palindrome_( str )
+  {
+    let ans =  palindrome(str);
+    if( ans == true )
+    {
+      console.log(" true" );
+    }
+    else
+    {
+      console.log("false");
+    }
+  }
+  // test variable
+  let m = 'ABCDCBA';
+  palindrome_(m);
+
+  document.getElementById("task3").innerHTML=`True. the right answer is in the console `
+
+
+
+//////////////////////////////////////////////////////
+
+console.log("task4");
+
+function decendingarray(arr){
+    for (let i=0 ; i<arr.length;i++)
+    for (let a=i+1; a<arr.length ; a++){
+        let b=0
+        if(arr[i]<arr[a]){
+          b=arr[i] ;
+          arr[i]=arr[a]
+          arr[a]=b
+
+        }
+    }
+
+    return arr
+}
+
+console.log( decendingarray([2,9,0,4]));
+
+
+document.write( decendingarray( [2,9,0,4]));
+
---
+++ task/app.js
@@ -0,0 +1,13 @@
+
+
+// function abc(x){
+//     let a =1;
+//     for(let i=0; i<x.length/2;++i){
+//         if(a[i]===a[a.length-x]){
+//             return true
+//         }else{
+//             return false
+//         }
+//     }
+// }
+// console.log(abc(x));
